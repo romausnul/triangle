73ba772e87dcd633460a62844a0c010f146e1890
РомаН <119739400+romausnul@users.noreply.github.com>
2022-12-06 22:12:24 +0300

    triangle

---
+++ triangle.cs
@@ -0,0 +1,35 @@
+    using System;
+
+    namespace triangle;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables and then initialize to zero.
+            int num1 = 0; int num2 = 0;
+
+            // Display title as the C# console calculator app.
+            Console.WriteLine("Дарова ккта");
+            Console.WriteLine("Введите величины первого треугольника");
+            Console.WriteLine("Введите величины второго треугольника");
+
+            // Ask the user to type the first number.
+            num1 = (a1+b1+c1);
+
+            // Ask the user to type the second number.
+            num2 = (a2+b2+c2);
+
+            // Ask the user to choose an option.
+            int x = ;
+            int y = ;
+            Console.WriteLine(System.Math.Round((double)(x * y / 100)));
+
+            // Use a switch statement to do the math.
+            switch (Console.ReadLine())
+            
+            // Wait for the user to respond before closing.
+            Console.Write("Ты помоему перепутал");
+            Console.ReadKey();
+        }
+    }
